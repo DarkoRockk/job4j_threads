eaba449a8309044174859f6e5033d8185816373d
darkorockk <shadrin.dev@gmail.com>
2021-07-01 21:57:05 +0300

    3.1. Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -7,11 +7,10 @@ public class Wget {
                     try {
                         for (int i = 1; i <= 100; i++) {
                             System.out.print("\rLoading : " + i + "%");
-                            ;
                             Thread.sleep(1000);
                         }
                     } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
                     }
                 }
         );
